bfc104bcae09561beb609deb488c5725debc9537
CarloMiranda <carlomiranda161@gmail.com>
2024-02-13 23:48:14 +0800

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Student Management System</title>
+    <style>
+        body {
+    margin: 0;
+    font-family: "Lato", sans-serif;
+  }
+  
+  .sidebar {
+    margin: 0;
+    padding: 0;
+    width: 200px;
+    background-color: #f1f1f1;
+    position: fixed;
+    height: 100%;
+    overflow: auto;
+  }
+  
+  .sidebar a {
+    display: block;
+    color: black;
+    padding: 16px;
+    text-decoration: none;
+  }
+   
+  .sidebar a.active {
+    background-color: #04AA6D;
+    color: white;
+  }
+  
+  .sidebar a:hover:not(.active) {
+    background-color: #555;
+    color: white;
+  }
+  
+  div.content {
+    margin-left: 200px;
+    padding: 1px 16px;
+    height: 1000px;
+  }
+  
+  @media screen and (max-width: 700px) {
+    .sidebar {
+      width: 100%;
+      height: auto;
+      position: relative;
+    }
+    .sidebar a {float: left;}
+    div.content {margin-left: 0;}
+  }
+  
+  @media screen and (max-width: 400px) {
+    .sidebar a {
+      text-align: center;
+      float: none;
+    }
+  }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#"><h1>Student Management System</h1></a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+                </nav>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md 12">
+                <!-- This is a sidebar -->
+                <div class="sidebar">
+                    <a class="active" href="#home">Home</a>
+                    <a href="{{ url('/students')}}">Student</a>
+                    <a href="{{ url('/teachers') }}">Teacher</a>
+                    <a href="{{ url('/courses') }}">Courses</a>
+                    <a href="{{ url('/batches') }}">Batches</a>
+                    <a href="{{ url('/enrollment') }}">Enrollment</a>
+                    <a href="{{ url('/payment')}}">Payment</a>
+                </div>
+            </div>
+            <div class="col-md-9">
+                    @yield('content')
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ resources/views/teachers/edit.blade.php
@@ -0,0 +1,21 @@
+@extends('layout')
+@section('content')
+<div class="card">
+    <div class="card-header">Edit Page</div>
+    <div class="card-body">
+
+    <form action="{{ url('teachers/' .$teachers->id) }}" method="post">
+    {!! csrf_field() !!}
+        @method("PATCH")
+        <input type="hidden" name="id" id="id" value="{{ $teachers->id }}" />
+        <label>Name</label> <br/>
+        <input type="text" name="name" id="name" value="{{ $teachers->name }}" class="form-control"> <br/>
+        <label>Address</label>
+        <input type="text" id="address" id="address" value="{{ $teachers->address }}" class="form-control"> <br/>
+        <label>Mobile</label>
+        <input type="text" name="mobile" id="mobile" value="{{ $teachers->mobile }}" class="form-control"> <br/>
+        <input type="submit" value="Update" class="btn btn-success"> <br/>
+    </form>
+    </div>
+</div>
+@stop
