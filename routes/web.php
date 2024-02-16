101eea6ecafd276a1f3e0e519b28acaaf130670b
CarloMiranda <carlomiranda161@gmail.com>
2024-02-16 23:01:13 +0800

    added print recipt for payment

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\BatchController;
 use App\Http\Controllers\EnrollmentController;
 use App\Http\Controllers\PaymentController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReportController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,3 +35,5 @@ Route::resource('/batches', BatchController::class);
 Route::resource('/enrollments', EnrollmentController::class);
 
 Route::resource('/payments', PaymentController::class);
+
+Route::get('report/report1/{pid}', [ReportController::class,'report1']);
